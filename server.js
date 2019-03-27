32464853db08e11c1311992e637afc43fd0be379
SnehaSadhwani <sneha.m.sadhwani@gmail.com>
2019-03-27 16:26:00 +0530

    setup for heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var dt = require('./myFirstModule');
-
+const port  = process.env.PORT||8080;
 
 http.createServer((req,res)=>{
     res.writeHead(200,
     {'Content-Type':'text/html'});
     res.write('The date and time are currently: '+ dt.myDateTime());
     res.end();
-}).listen(8080);
+}).listen(port,()=>{
+    console.log(`Server is up on port ${port}`)
+});
